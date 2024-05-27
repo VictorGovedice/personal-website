95282d7a29a30067d46f77e974d5c2c69f0bdae1
Victor Govedice <69945592+VictorGovedice@users.noreply.github.com>
2024-05-27 20:28:48 -0300

    Update Projects.js
    Adicionei o link do github e o seu icone

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaGithub } from 'react-icons/fa';
 
 import Main from '../layouts/Main';
-
 import Cell from '../components/Projects/Cell';
 import data from '../data/projects';
 
@@ -15,7 +15,12 @@ const Projects = () => (
       <header>
         <div className="title">
           <h2><Link to="/projects">Projetos</Link></h2>
-          <p>A seleção de alguns projetos.</p>
+          <p>Todos os projetos estão no GitHub. clique no gatinho</p>
+          <p>
+            <a href="https://github.com/VictorGovedice?tab=repositories" target="_blank" rel="noopener noreferrer">
+              <FaGithub size={30} />
+            </a>
+          </p>
         </div>
       </header>
       {data.map((project) => (
